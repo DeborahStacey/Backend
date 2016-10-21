9596cf5c55d4b6fe977a9ea635b376a5291fc92a
Devin D <ddagg@mail.uoguelph.ca>
2016-10-21 01:45:15 -0400

    removing temp info from file

--- config/configFile.php
+++ config/configFile.php
@@ -1,12 +1,12 @@
 <?php
     $app['config'] = array(
         'database' => array(
-            'host'   => 'welcat-working.cz3evk2oqbiz.us-west-2.rds.amazonaws.com',
-            'user'   => 'welcat',
-            'pass'   => 'Welcat123',
-            'dbname' => 'WelcatWorking'
+            'host'   => '',
+            'user'   => '',
+            'pass'   => '',
+            'dbname' => ''
         ),
         'crypto' => array (
-            'salt' => 'H5kcaVA2ZLChgV879c8F49TrH23ctF'
+            'salt' => ''
         )
     );
