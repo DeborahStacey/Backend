2b7b50f76c2c2a49cc46c33b57516bc51b1f325b
mwilso14 <mwilso14@mail.uoguelph.ca>
2016-10-25 13:38:38 -0400

    adding address endpoints

---
+++ src/Controllers/AddressController.php
@@ -0,0 +1,67 @@
+<?php
+namespace WellCat\Controllers;
+
+use PDO;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use WellCat\JsonResponse;
+
+class AddressController
+{
+    protected $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function Countries()
+    {
+        $sql ='SELECT countryid AS id, countryname AS name FROM country';
+
+        $stmt= $this->app['db']->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        if($result){
+            $body = array( 'countries' => $result);
+            $body['success'] = 'true';
+            return new JsonResponse($body, 200);
+        }
+        else{
+                $body = array( 
+                        'success' => false,
+                        'error' => 'no results for that id'   
+                );
+
+                return new JsonResponse($body, 404);
+            }
+
+    }
+
+    public function Locations(Request $request, $city)
+    {
+
+        $sql ='SELECT locationid AS id, locationname AS name FROM location WHERE countryid = :countryID';
+
+        $stmt= $this->app['db']->prepare($sql);
+        $stmt->execute(array( ':countryID' => $city));
+
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        
+        if($result){
+                $body = array( 'locations' => $result);
+                $body['success'] = 'true';
+                return new JsonResponse($body, 200);
+        }
+        else{
+            $body = array( 
+                'success' => false,
+                'error' => 'no results for that id' 
+                );
+
+                return new JsonResponse($body, 404);
+        }
+    }
+}
