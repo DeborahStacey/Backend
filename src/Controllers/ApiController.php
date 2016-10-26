533de747d38d886095d42844f0c7e55e5c1c8d23
Cory Andrews <candre05@mail.uoguelph.ca>
2016-10-26 09:50:25 -0400

    Updated ApiController to include animal endpoints

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -18,8 +18,10 @@ class ApiController
     {
         $data = array(
             '/' => 'Get end points',
-            '/address/countries' => 'Gets list of countrys and id\'s',
+            '/address/countries' => 'Gets list of countries and id\'s',
             '/address/{countryID}/locations' => 'Gets list of locations in a given country',
+            '/animal/animals' => 'Gets list of animals and id\'s',
+            '/animal/{animalID}/breeds' => 'Gets list of breeds associated with a given animal',
             '/user/authenticate' => 'Checks to see if current user is authenticated/logged in'
         );
         return new JsonResponse($data, 201);
