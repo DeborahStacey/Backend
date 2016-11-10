01b3c375f6b59cbcccaed895bbacf663e7401fad
Devin D <ddagg@mail.uoguelph.ca>
2016-11-10 09:19:15 -0500

    fixed issue with getting pets and needing both a personal and shared pet to get a list. Also added breed to the get pet(s) responses

--- src/Controllers/PetController.php
+++ src/Controllers/PetController.php
@@ -192,7 +192,7 @@ class PetController
             }
         }
 
-        $sql = 'SELECT name, breedid AS breedID, gender, dateofbirth AS dateOfBirth, weight, height, length FROM pet WHERE petid = :petID';
+        $sql = 'SELECT name, gender, breed, dateofbirth, weight, height, length FROM pet WHERE petid = :petID';
         $stmt = $this->app['db']->prepare($sql);
         $stmt->execute(array(
             ':petID' => $petID
@@ -214,7 +214,7 @@ class PetController
         $user = $this->app['session']->get('user');
 
         //get list of all pets that the current user owns
-        $sql = 'SELECT p.petid AS petID, p.name, p.gender, a.firstname, a.lastname, p.lastupdated AS lastUpdated FROM pet p INNER JOIN account a ON p.ownerid = a.userid WHERE p.ownerid = :user';
+        $sql = 'SELECT p.petid, p.name, p.gender, p.breed, a.firstname, a.lastname, p.lastupdated FROM pet p INNER JOIN account a ON p.ownerid = a.userid WHERE p.ownerid = :user';
 
         $stmt = $this->app['db']->prepare($sql);
         $stmt->execute(array(
@@ -224,7 +224,7 @@ class PetController
         $personal = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
         //get list of all pets that current user has access to.
-        $sql = 'SELECT p.petid AS petID, p.name, p.gender, a.firstname, a.lastname, p.lastupdated AS lastUpdated FROM pet p INNER JOIN account a ON p.ownerid = a.userid WHERE p.petid IN (SELECT f.petid FROM accessibility f WHERE f.userid = :user)';
+        $sql = 'SELECT p.petid, p.name, p.gender, p.breed, a.firstname, a.lastname, p.lastupdated FROM pet p INNER JOIN account a ON p.ownerid = a.userid WHERE p.petid IN (SELECT f.petid FROM accessibility f WHERE f.userid = :user)';
 
         $stmt = $this->app['db']->prepare($sql);
         $stmt->execute(array(
@@ -233,7 +233,7 @@ class PetController
 
         $shared = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
-        if ($personal && $shared) {
+        if ($personal || $shared) {
             $body = array(
                 'success' => true,
                 'personal' => $personal,
