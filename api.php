57be6e29e768d76de900a29c75ae232fc3ecab0b
Devin D <ddagg@mail.uoguelph.ca>
2016-12-01 09:17:51 -0500

    fixed access methods

--- api.php
+++ api.php
@@ -6,6 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(-1);
 
 header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Methods: GET,PUT,DELETE,POST');
 require_once __DIR__.'/vendor/autoload.php';
 
 use WellCat\Providers\ApiControllerProvider;
